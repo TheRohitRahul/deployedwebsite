02c4582561b36326ddc22c83471f994269d2076e
Rohit Rahul <rohit.rahul@tcs.com>
2019-04-29 00:57:53 +0530

    Updates

---
+++ precache-manifest.0d86ec77c0beb8216373d64446db20c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5850176a8d773924caaeef68fe5e4c99",
+    "url": "/deployedwebsite/index.html"
+  },
+  {
+    "revision": "7508a2a2216888b53381",
+    "url": "/deployedwebsite/static/css/main.e0e70ba6.chunk.css"
+  },
+  {
+    "revision": "40d9902ea84a441c5607",
+    "url": "/deployedwebsite/static/js/2.f0ac6a99.chunk.js"
+  },
+  {
+    "revision": "7508a2a2216888b53381",
+    "url": "/deployedwebsite/static/js/main.a0eca663.chunk.js"
+  },
+  {
+    "revision": "689883fb0c01fa3d1114",
+    "url": "/deployedwebsite/static/js/runtime~main.97724fa5.js"
+  }
+]);
